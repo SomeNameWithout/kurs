d66571a9d40b84fe03f777ddf44e7e63a915a051
Bannn <starostin_g@mail.ru>
2015-05-23 20:00:02 +0300

    проблемы с 2012, поэтому перешёл на 2008; добавлены классы, отражающие представление записей в бд

---
+++ kurs2008/kurs2008/BDClasses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kurs2008
+{
+    class Employee
+    {
+        public int ID;
+        public string FIO;
+        public int Burden;
+    }
+    class Project
+    {
+        public int ID;
+        public string Name;
+    }
+    class Task
+    {
+        public int ID;
+        public string Name;
+        public bool State;
+        public int TaskType_ID;
+        public int Volume;
+        public string Limitation_date;
+        public string Completion_date;
+        public int Proj_ID;
+        public int Empl_ID;
+    }
+    class Salary
+    {
+        public int ID;
+        public int Empl_ID;
+        public int Date;
+        public int Volume;
+    }
+    class TaskType
+    {
+        public int ID;
+        public int Speed;//нормативная скорость выполнения
+        public int Complexity;//коэффициент сложности
+    }
+    class SalaryCalculationVariables
+    {
+    }
+
+}
---
+++ kurs2008/kurs2008/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kurs2008
+{
+    class Settings
+    {
+        public string s;
+    }
+}
